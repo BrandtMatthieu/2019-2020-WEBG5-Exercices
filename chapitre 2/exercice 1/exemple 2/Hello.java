1a0e032c04606dc167a0ddb14dd18c6521b657bf
Matthieu Brandt <44422@etu.he2b.be>
2020-09-15 16:50:44 +0200

    chapire 2.1 exercice 1

---
+++ chapitre 2/exercice 1/exemple 2/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    /**
+     * Main method
+     * 
+     * @param String[] an array of arguments
+     * @return void
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
