8779bd896509c895e68441cdcc4b3c16c1878234
BrandtMatthieu <44422@etu.he2b.be>
2020-12-30 11:33:24 +0100

    chapitre 2.2 exercice 5

---
+++ chapitre 2/exercice 5/HelloWorld/src/main/java/webg5/maven/App.java
@@ -0,0 +1,17 @@
+package webg5.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        App.printLine( "Hello World!" );
+    }
+
+    public static void printLine(String message) {
+        System.out.println( message );
+    }
+}
