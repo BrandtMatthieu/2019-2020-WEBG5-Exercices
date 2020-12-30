d6b1c16fdd4d570fdbfdd56a09f748e1a835c3d0
BrandtMatthieu <44422@etu.he2b.be>
2020-12-30 11:33:53 +0100

    chapitre 3.1 tutoriel 1

---
+++ chapitre 3/Hello/src/main/java/webg5/vscode/App.java
@@ -0,0 +1,20 @@
+package webg5.vscode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		System.out.println( "Hello World!" );
+		System.out.println();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}
