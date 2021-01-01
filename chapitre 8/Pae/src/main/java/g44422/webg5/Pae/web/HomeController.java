f8f2be2956fb5e445fdc9cb499824a5e9a572d5d
BrandtMatthieu <44422@etu.he2b.be>
2021-01-01 17:07:56 +0100

    chapitre 8.1 exercice 1

---
+++ chapitre 8/Pae/src/main/java/g44422/webg5/Pae/web/HomeController.java
@@ -0,0 +1,14 @@
+package g44422.webg5.Pae.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+
+}
